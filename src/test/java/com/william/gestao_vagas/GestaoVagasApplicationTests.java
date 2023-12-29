4557935b0380a2c976ba876c33fad740de1c7a67
William da Cruz Pires <williamcruzpires@alunos.utfpr.edu.br>
2023-12-29 20:17:30 -0300

    Validação de campos

---
+++ src/test/java/com/william/gestao_vagas/GestaoVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.william.gestao_vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
