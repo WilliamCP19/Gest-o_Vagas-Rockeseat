7330358aeda760c08a71b7d3d776b42ae2c72f8f
William da Cruz Pires <williamcruzpires@alunos.utfpr.edu.br>
2024-01-06 12:34:50 -0300

    Criando CandidateRepository

---
+++ src/main/java/com/william/gestao_vagas/modules/candidate/repository/CandidateRepository.java
@@ -0,0 +1,11 @@
+package com.william.gestao_vagas.modules.candidate.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.william.gestao_vagas.modules.candidate.entities.Candidate;
+
+public interface CandidateRepository extends JpaRepository <Candidate, UUID>{
+    Optional<Candidate> findByUsernameOrEmail(String username, String email);
+}
