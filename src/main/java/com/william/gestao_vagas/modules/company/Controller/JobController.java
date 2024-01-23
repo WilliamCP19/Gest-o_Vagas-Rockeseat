23ad7d575e1d3e683e05a05c1e6ff584267d2a73
William da Cruz Pires <williamcruzpires@alunos.utfpr.edu.br>
2024-01-23 15:36:09 -0300

    Inserindo Segurança na Entidade Job

--- src/main/java/com/william/gestao_vagas/modules/company/Controller/JobController.java
+++ src/main/java/com/william/gestao_vagas/modules/company/Controller/JobController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.william.gestao_vagas.modules.company.dto.CreateJobDTO;
 import com.william.gestao_vagas.modules.company.entities.Job;
 import com.william.gestao_vagas.modules.company.services.CreateJobService;
 
@@ -23,9 +24,16 @@ public class JobController {
     private CreateJobService createJobUserCase;
 
     @PostMapping("/")
-    public Job create (@Valid @RequestBody Job job, HttpServletRequest request) {
+    public Job create (@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
         var companyId = request.getAttribute("company_id");
-        job.setCompanyId(UUID.fromString(companyId.toString()));
+        //job.setCompanyId(UUID.fromString(companyId.toString()));
+        
+        var job = Job.builder()
+        .benefits(createJobDTO.getBenefits())
+        .companyId(UUID.fromString(companyId.toString()))
+        .description(createJobDTO.getDescription())
+        .level(createJobDTO.getLevel())
+        .build();
         return createJobUserCase.execute(job);
     }
 }
---
+++ src/main/java/com/william/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -0,0 +1,10 @@
+package com.william.gestao_vagas.modules.company.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateJobDTO {
+    private String description;
+    private String benefits;
+    private String level;
+}
