a1ce5254f0c08eed0a51bd49f9021fb82a478ca6
William da Cruz Pires <williamcruzpires@alunos.utfpr.edu.br>
2024-03-13 16:56:11 -0300

    Configurando Swagger para Doc

--- src/main/java/com/william/gestao_vagas/GestaoVagasApplication.java
+++ src/main/java/com/william/gestao_vagas/GestaoVagasApplication.java
@@ -3,11 +3,19 @@ package com.william.gestao_vagas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class GestaoVagasApplication {
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Gestão de Vagas",
+		description = "API responsável pela gestão de vagas",
+		version = "1"
+	)
+)
+public class GestaoVagasApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GestaoVagasApplication.class, args);
 	}
-
 }
